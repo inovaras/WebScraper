2527a19fd1e49e488e45ae80e25dd7e77cf97ff9
Альбина Гильмутдинова <inovaras@mail.ru>
2023-12-26 23:52:26 +0500

    admin panel: add filter by author post

--- web_scraper/parser/admin.py
+++ web_scraper/parser/admin.py
@@ -6,7 +6,7 @@ from .models import Article
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'date', 'article_url', 'author_name', 'author_url', 'text')
     search_fields = ('pk', 'title', 'author_name', 'text')
-    list_filter = ('date', )
+    list_filter = ('date', 'author_name', )
     empty_value_display = '-нет_данных-'
 
 
