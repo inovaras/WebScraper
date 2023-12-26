ae3a5cf1c5e62ec570d1b5d0449b58376ae0c4db
Альбина Гильмутдинова <inovaras@mail.ru>
2023-12-26 17:21:52 +0500

    startapp parser

--- web_scraper/parser/parser.py
+++ web_scraper/parser/parser.py
@@ -8,30 +8,20 @@ from bs4 import BeautifulSoup as bs
 def get_main_page():
     url = 'https://habr.com/ru/'
     response = requests.get(url)
-    with open("info/main.txt", "w", encoding="utf-8") as file:
-        file.write(response.text)
     return response.text
 
 
 def parse_main_page(html):
     soup = bs(html, 'html.parser')
     hub_links = soup.find_all('a', class_="tm-title__link")
-    with open("info/main_info.txt", "w", encoding="utf-8") as file:
-        for link in hub_links:
-            file.write(f"hub_link: {link}\n")
-
     article_links = ["https://habr.com"+link['href'] for link in hub_links]
-    with open("info/article_links.txt", "a", encoding="utf-8") as file:
-        for link in article_links:
-            file.write(f"hub_link: {link}\n")
     return article_links
 
 
 def parse_article_page(article_url):
     response = requests.get(article_url)
-    with open("info/article.txt", "w", encoding="utf-8") as file:
-        file.write(response.text)
-    print(response.status_code)
+    if response.status_code != 200:
+        print(response.status_code)
     soup = bs(response.text, 'html.parser')
     script_content = soup.find('script', {'data-vue-meta': 'ssr', 'type': 'application/ld+json'}).string
     author_content = soup.find('a', class_='tm-user-info__username')
@@ -50,21 +40,33 @@ def parse_article_page(article_url):
     }
 
 
+def print_article_info(info):
+    print("\nArticle Information:")
+    print(f"Title: {info['title']}")
+    print(f"Date: {info['date']}")
+    print(f"URL: {info['article_url']}")
+    print(f"Author: {info['author_name']}")
+    print(f"Author URL: {info['author_url']}")
+    print("-" * 50)
+
+
 def main():
     while True:
         print("Получаю данные с Хабра...")
         main_page_html = get_main_page()
         article_links = parse_main_page(main_page_html)
+        articles = []
 
         for article_link in article_links:
-            article_info = parse_article_page(article_link)
-            print("\nArticle Information:")
-            print(f"Title: {article_info['title']}")
-            print(f"Date: {article_info['date']}")
-            print(f"URL: {article_info['article_url']}")
-            print(f"Author: {article_info['author_name']}")
-            print(f"Author URL: {article_info['author_url']}")
-            print("-" * 50)
+            info = parse_article_page(article_link)
+            # print_article_info(info)
+            articles_info = dict()
+            articles_info['title'] = info['title']
+            articles_info['date'] = info['date']
+            articles_info['article_url'] = info['article_url']
+            articles_info['author_name'] = info['author_name']
+            articles_info['author_url'] = info['author_url']
+            articles.append(articles_info)
 
         print("Жду 10 минут...")
         time.sleep(600)  # Wait for 10 minutes
