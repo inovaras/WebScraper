95b9fe185b08004e20c68c4e43b2b716a2621a2e
Альбина Гильмутдинова <inovaras@mail.ru>
2023-12-27 21:48:01 +0500

    printing logs using logging+ use linter to  all files

--- web_scraper/parser/management/commands/async_parse_hub_and_fill_bd.py
+++ web_scraper/parser/management/commands/async_parse_hub_and_fill_bd.py
@@ -1,14 +1,26 @@
-import json
-import time
 import asyncio
+import json
+import logging
+import time
+import sys
+
 import aiohttp
 from bs4 import BeautifulSoup as bs
 from django.core.management.base import BaseCommand
 
 from ...models import Article
 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(stream=sys.stdout)
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
 class Command(BaseCommand):
-    help = 'Передача данных в базу данных'
+    help = "Передача данных в базу данных"
     articles = []
 
     async def fetch(self, session, url):
@@ -16,54 +28,62 @@ class Command(BaseCommand):
             return await response.text()
 
     async def get_main_page(self):
-        url = 'https://habr.com/ru/'
+        url = "https://habr.com/ru/"
         async with aiohttp.ClientSession() as session:
             html = await self.fetch(session, url)
         return html
 
     async def parse_main_page(self, html):
-        soup = bs(html, 'html.parser')
-        hub_links = soup.find_all('a', class_="tm-title__link")
-        article_links = ["https://habr.com" + link['href'] for link in hub_links]
+        soup = bs(html, "html.parser")
+        hub_links = soup.find_all("a", class_="tm-title__link")
+        article_links = ["https://habr.com" + link["href"] for link in hub_links]
         return article_links
 
     async def parse_article_page(self, session, article_url):
         async with session.get(article_url) as response:
             if response.status != 200:
-                print(response.status)
+                logger.error(response.status)
                 return None
 
-            soup = bs(await response.text(), 'html.parser')
+            soup = bs(await response.text(), "html.parser")
 
-            script_content = soup.find('script', {'data-vue-meta': 'ssr', 'type': 'application/ld+json'}).string
-            author_content = soup.find('a', class_='tm-user-info__username')
+            script_content = soup.find(
+                "script", {"data-vue-meta": "ssr", "type": "application/ld+json"}
+            ).string
+            author_content = soup.find("a", class_="tm-user-info__username")
             data = json.loads(script_content)
-            title = data.get('headline', '')
-            date = data.get('datePublished', '')
-            author_name = data.get('author', '').get('name')
+            title = data.get("headline", "")
+            date = data.get("datePublished", "")
+            author_name = data.get("author", "").get("name")
             if author_content:
-                author_url = 'https://habr.com' + author_content.get('href')
+                author_url = "https://habr.com" + author_content.get("href")
             else:
-                author_url = 'empty'
+                author_url = "empty"
 
-            post = soup.find('div', class_='article-formatted-body article-formatted-body article-formatted-body_version-2')
+            post = soup.find(
+                "div",
+                class_="article-formatted-body article-formatted-body article-formatted-body_version-2",
+            )
             if post:
                 text = post.text
             else:
-                text = 'empty'
+                text = "empty"
 
             return {
-                'title': title,
-                'date': date,
-                'article_url': article_url,
-                'author_name': author_name,
-                'author_url': author_url,
-                'text': text
+                "title": title,
+                "date": date,
+                "article_url": article_url,
+                "author_name": author_name,
+                "author_url": author_url,
+                "text": text,
             }
 
     async def parse_articles(self, article_links):
         async with aiohttp.ClientSession() as session:
-            tasks = [self.parse_article_page(session, article_link) for article_link in article_links]
+            tasks = [
+                self.parse_article_page(session, article_link)
+                for article_link in article_links
+            ]
             return await asyncio.gather(*tasks)
 
     async def async_parse(self):
@@ -73,24 +93,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         while True:
-            print("Получение данных с Хабра...")
+            logger.info("Получение данных с Хабра")
             asyncio.run(self.async_parse())
-            print('Перенос обновлений в БД')
+            logger.info("Перенос обновлений в БД")
 
             for article_data in self.articles:
                 if article_data:
-                    existing_article = Article.objects.filter(article_url=article_data['article_url']).first()
+                    existing_article = Article.objects.filter(
+                        article_url=article_data["article_url"]
+                    ).first()
 
                     if not existing_article:
-
                         Article.objects.create(
-                            title=article_data['title'],
-                            date=article_data['date'],
-                            article_url=article_data['article_url'],
-                            author_name=article_data['author_name'],
-                            author_url=article_data['author_url'],
-                            text=article_data['text']
+                            title=article_data["title"],
+                            date=article_data["date"],
+                            article_url=article_data["article_url"],
+                            author_name=article_data["author_name"],
+                            author_url=article_data["author_url"],
+                            text=article_data["text"],
                         )
 
-            print("Ожидание 10 минут...")
+            logger.info("Ожидание 10 минут")
             time.sleep(600)
--- web_scraper/parser/management/commands/parse_hub_and_fill_bd.py
+++ web_scraper/parser/management/commands/parse_hub_and_fill_bd.py
@@ -1,5 +1,6 @@
 import json
 import time
+
 import requests
 from bs4 import BeautifulSoup as bs
 from django.core.management.base import BaseCommand
@@ -8,20 +9,20 @@ from ...models import Article
 
 
 class Command(BaseCommand):
-    help = 'Передача данных в базу данных'
+    help = "Передача данных в базу данных"
     articles = []
 
     @staticmethod
     def get_main_page():
-        url = 'https://habr.com/ru/'
+        url = "https://habr.com/ru/"
         response = requests.get(url)
         return response.text
 
     @staticmethod
     def parse_main_page(html):
-        soup = bs(html, 'html.parser')
-        hub_links = soup.find_all('a', class_="tm-title__link")
-        article_links = ["https://habr.com" + link['href'] for link in hub_links]
+        soup = bs(html, "html.parser")
+        hub_links = soup.find_all("a", class_="tm-title__link")
+        article_links = ["https://habr.com" + link["href"] for link in hub_links]
         return article_links
 
     @staticmethod
@@ -29,31 +30,36 @@ class Command(BaseCommand):
         response = requests.get(article_url)
         if response.status_code != 200:
             print(response.status_code)
-        soup = bs(response.text, 'html.parser')
+        soup = bs(response.text, "html.parser")
 
-        script_content = soup.find('script', {'data-vue-meta': 'ssr', 'type': 'application/ld+json'}).string
-        author_content = soup.find('a', class_='tm-user-info__username')
+        script_content = soup.find(
+            "script", {"data-vue-meta": "ssr", "type": "application/ld+json"}
+        ).string
+        author_content = soup.find("a", class_="tm-user-info__username")
         data = json.loads(script_content)
-        title = data.get('headline', '')
-        date = data.get('datePublished', '')
-        author_name = data.get('author', '').get('name')
+        title = data.get("headline", "")
+        date = data.get("datePublished", "")
+        author_name = data.get("author", "").get("name")
         if author_content:
-            author_url = 'https://habr.com' + author_content.get('href')
+            author_url = "https://habr.com" + author_content.get("href")
         else:
-            author_url = 'empty'
-        post = soup.find('div', class_='article-formatted-body article-formatted-body article-formatted-body_version-2')
+            author_url = "empty"
+        post = soup.find(
+            "div",
+            class_="article-formatted-body article-formatted-body article-formatted-body_version-2",
+        )
         if post:
             text = post.text
         else:
-            text = 'empty'
+            text = "empty"
 
         return {
-            'title': title,
-            'date': date,
-            'article_url': article_url,
-            'author_name': author_name,
-            'author_url': author_url,
-            'text': text
+            "title": title,
+            "date": date,
+            "article_url": article_url,
+            "author_name": author_name,
+            "author_url": author_url,
+            "text": text,
         }
 
     @staticmethod
@@ -72,32 +78,34 @@ class Command(BaseCommand):
         for article_link in article_links:
             info = Command.parse_article_page(article_link)
             articles_info = dict()
-            articles_info['title'] = info['title']
-            articles_info['date'] = info['date']
-            articles_info['article_url'] = info['article_url']
-            articles_info['author_name'] = info['author_name']
-            articles_info['author_url'] = info['author_url']
-            articles_info['text'] = info['text']
+            articles_info["title"] = info["title"]
+            articles_info["date"] = info["date"]
+            articles_info["article_url"] = info["article_url"]
+            articles_info["author_name"] = info["author_name"]
+            articles_info["author_url"] = info["author_url"]
+            articles_info["text"] = info["text"]
             self.articles.append(articles_info)
 
     def handle(self, *args, **options):
         while True:
             print("Получаю данные с Хабра...")
             self.parse()
-            print('Обновляю данные в БД')
+            print("Обновляю данные в БД")
 
             for article_data in self.articles:
                 if article_data:
-                    existing_article = Article.objects.filter(article_url=article_data['article_url']).first()
+                    existing_article = Article.objects.filter(
+                        article_url=article_data["article_url"]
+                    ).first()
 
                     if not existing_article:
                         Article.objects.create(
-                            title=article_data['title'],
-                            date=article_data['date'],
-                            article_url=article_data['article_url'],
-                            author_name=article_data['author_name'],
-                            author_url=article_data['author_url'],
-                            text=article_data['text']
+                            title=article_data["title"],
+                            date=article_data["date"],
+                            article_url=article_data["article_url"],
+                            author_name=article_data["author_name"],
+                            author_url=article_data["author_url"],
+                            text=article_data["text"],
                         )
 
             print("Жду 10 минут...")
